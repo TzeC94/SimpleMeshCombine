89a5e7c6dcf91b707efd16e4f9bcaeca0254cc40
Tze Chian <teoh_0827@hotmail.com>
2021-02-22 21:10:55 +0800

    First Commit

---
+++ Packages/RIK Mesh Combine/Editor/RIKMeshCombineEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using RIK.MeshCombine;
+
+[CustomEditor(typeof(RIKMeshCombineScript))]
+public class RIKMeshCombineEditor : Editor{
+
+    public override void OnInspectorGUI(){
+
+        var meshCombineTarget = (RIKMeshCombineScript)target;
+        
+        if(GUILayout.Button("Combine"))
+            meshCombineTarget.Run();
+
+    }
+   
+}
---
+++ Packages/RIK Mesh Combine/Script/RIKMeshCombineScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace RIK.MeshCombine{
+
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class RIKMeshCombineScript : MonoBehaviour{
+        
+        // Start is called before the first frame update
+        public void Run(){
+
+            Vector3 oriPos = transform.position;
+            transform.position = Vector3.zero;
+
+            int i = 1;
+
+            MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+            CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+            
+            while (i < meshFilters.Length)
+            {
+                combine[i].mesh = meshFilters[i].sharedMesh;
+                combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+                meshFilters[i].gameObject.SetActive(false);
+
+                i++;
+            }
+
+            transform.GetComponent<MeshFilter>().mesh = new Mesh();
+            transform.GetComponent<MeshFilter>().sharedMesh.CombineMeshes(combine, true, true, true);
+            transform.GetComponent<Renderer>().sharedMaterials = meshFilters[1].gameObject.GetComponent<Renderer>().sharedMaterials;
+            transform.gameObject.SetActive(true);
+
+            transform.position = oriPos;
+
+        }
+
+    }
+
+}
